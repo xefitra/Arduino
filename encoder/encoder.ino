82bbac589f07b75a760e56df522759225891a639
xefitra <artifexdepot@gmail.com>
2018-10-04 21:44:37 -0500

    Se agrega la variable theta para expresar los datos en radianes.

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,14 +1,24 @@
 //Script .ino para sensar posición con encoder incremental de 600 p/r
 float temp, counter = 0;
-    
+int sentido = 0;
+String sent;
+float theta = 0;
+
 void setup() {
   Serial.begin (9600);
 
   pinMode(2, INPUT_PULLUP); // Pin de entrada, cable blanco del encoder
   
   pinMode(3, INPUT_PULLUP); // Pin de entrada, cable verde del encoder
-  // Para calcular los pasos se procede a establecer funciones de interupccion en los pines de entrada
-  
+
+/*  En este caso se utiliza solo uno de los pines pues queremos evaluar la posición, 
+ *  al utilizar los dos se tendría más resolución pero para la aplicación que se 
+ *  planea esta resolución esta bien, posterior a este script estará uno para realizar 
+ *  la medición de velocidad angular "omega" en el cual se medirá el tiempo entre interrupciones
+ *  para calcular omega = theta/tiempo, theta = pasos * 2*pi / 600
+
+
+*/
   attachInterrupt(0, ai0, RISING);
    
 //  attachInterrupt(1, ai1, RISING);
@@ -17,28 +27,32 @@ void setup() {
   void loop() {
   // Seteando el valor del contador
   if( counter != temp ){
-  Serial.println (counter);
+  Serial.println (theta);
+
+  // Si se quiere imprimir el sentido de giro se proponen las variables sentido y sent.
+  
+//  if(sentido==1)
+//  {
+//    sent = "Horario";
+//    }
+//    else
+//    {
+//      sent = "Antihorario";
+//      }
+//  Serial.println(sent);
+
   temp = counter;
   }
   }
    
   void ai0() {
-  // Se activa la funcion por la señal en el pin 2
-  // Se verifica la condicion del pin 3 pra establecer la direccion
   if(digitalRead(3)==LOW) {
   counter++;
+  sentido=0;
   }else{
   counter--;
+  sentido=1;
+  }
+  theta = (counter * 2 * 3.1416) / 600;
   }
 
-  }
-   
-//  void ai1() {
-//  // Se activa la funcion por la señal en el pin 3 
-//  // Se verifica la condicion del pin 2 para establecer la direccion
-//  if(digitalRead(2)==LOW) {
-//  counter--;
-//  }else{
-//  counter++;
-//  }
-// }
