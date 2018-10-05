1b790962b8d9185d21629937f0298f850b3b9d6a
xefitra <artifexdepot@gmail.com>
2018-10-04 20:48:44 -0500

    Código

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,9 +1,43 @@
+//Script .ino para sensar posición con encoder incremental de 600 p/r
+volatile unsigned int temp, counter = 0;
+    
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin (9600);
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  pinMode(2, INPUT_PULLUP); // Pin de entrada, cable blanco del encoder
+  
+  pinMode(3, INPUT_PULLUP); // Pin de entrada, cable verde del encoder
+  // Para calcular los pasos se procede a establecer funciones de interupccion en los pines de entrada
+  
+  attachInterrupt(0, ai0, RISING);
+   
+  attachInterrupt(1, ai1, RISING);
+  }
+   
+  void loop() {
+  // Seteando el valor del contador
+  if( counter != temp ){
+  Serial.println (counter);
+  temp = counter;
+  }
+  }
+   
+  void ai0() {
+  // Se activa la funcion por la señal en el pin 2
+  // Se verifica la condicion del pin 3para establecer la direccion
+  if(digitalRead(3)==LOW) {
+  counter++;
+  }else{
+  counter--;
+  }
+  }
+   
+  void ai1() {
+  // Se activa la funcion por la señal en el pin 3 
+  // Se verifica la condicion del pin 2 para establecer la direccion
+  if(digitalRead(2)==LOW) {
+  counter--;
+  }else{
+  counter++;
+  }
 }
